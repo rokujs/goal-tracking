d6760bb2c59e5fccb200d1f410e998910a0988b2
Sebastian Gil <shiuxcode@gmail.com>
2022-03-05 20:19:59 -0500

    Create custom hook for API methods

--- src/components/ListOfGoals/index.jsx
+++ src/components/ListOfGoals/index.jsx
@@ -1,114 +1,15 @@
 import React, { useState, useEffect } from 'react'
 
+import { useGoal } from '@/hooks/useGoal'
+
 import Goal from '../Goal'
 import Loading from '../Loading'
-import getGoals from '@/services/getGoals'
 import remaining, { TimeDiff } from '@/utils/Date'
 
 function ListOfGoals () {
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
-  const [goals, setGoals] = useState([])
+  const { ResumeGoal, Done, Drop, goals, loading, error } = useGoal()
+
   const NOW = new Date()
-
-  // const RemoveGoal = goalId => {
-  //   setGoals(goals => {
-  //     const index = goals.findIndex(goal => goal._id === goalId)
-  //     if (index > -1) goals.splice(index, 1)
-
-  //     return [...goals]
-  //   })
-  // }
-
-  const ResumeGoal = ({ goalId, dateEnd }) => {
-    // fetch
-    const dataDate = {
-      newStart: Date.now(),
-      newEnd: dateEnd
-    }
-
-    const requestOptions = {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(dataDate)
-    }
-
-    fetch(`http://localhost:8080/goal/resume/${goalId}`, requestOptions)
-      .then(response => response.json())
-      .then(() => {
-        setGoals(goals => {
-          const goalDone = goals.findIndex(goal => goal._id === goalId)
-          goals[goalDone].end = false
-          goals[goalDone].todayDone = false
-
-          return [...goals]
-        })
-      })
-      .catch(err => console.error(err))
-  }
-
-  const Done = goalId => {
-    // fetch
-    const requestOptions = {
-      method: 'PATCH',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ todayDone: true })
-    }
-
-    fetch(`http://localhost:8080/goal/${goalId}`, requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        setGoals(goals => {
-          const goalDone = goals.findIndex(goal => goal._id === goalId)
-          goals[goalDone].todayDone = true
-
-          return [...goals]
-        })
-      })
-      .catch(err => console.error(err))
-  }
-
-  const Drop = ({ goalId, start }) => {
-    const dataGoalAbandon = {
-      end: true,
-      newTries: {
-        start,
-        end: new Date()
-      }
-    }
-    const requestOptions = {
-      method: 'PUT',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(dataGoalAbandon)
-    }
-
-    fetch(`http://localhost:8080/goal/${goalId}`, requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        setGoals(goals => {
-          const goalDone = goals.findIndex(goal => goal._id === goalId)
-          goals[goalDone].tries = data.tries
-          goals[goalDone].end = true
-
-          return [...goals]
-        })
-      })
-      .catch(err => console.error(err))
-  }
-
-  useEffect(() => {
-    getGoals()
-      .then(data => {
-        setLoading(false)
-        setError(false)
-        setGoals(data)
-      })
-      .catch(err => {
-        console.error(err)
-        setLoading(false)
-        setError(true)
-      })
-  }, [])
 
   if (loading) {
     return <Loading />
---
+++ src/hooks/useGoal.js
@@ -0,0 +1,116 @@
+import { useState, useEffect } from 'react'
+import getGoals from '@/services/getGoals'
+
+export const useGoal = () => {
+  const [goals, setGoals] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [error, setError] = useState(false)
+
+  useEffect(() => {
+    getGoals()
+      .then(data => {
+        setLoading(false)
+        setError(false)
+        setGoals(data)
+      })
+      .catch(err => {
+        console.error(err)
+        setLoading(false)
+        setError(true)
+      })
+  }, [])
+
+  const RemoveGoal = goalId => {
+    setGoals(goals => {
+      const index = goals.findIndex(goal => goal._id === goalId)
+      if (index > -1) goals.splice(index, 1)
+
+      return [...goals]
+    })
+  }
+
+  const ResumeGoal = ({ goalId, dateEnd }) => {
+    // fetch
+    const dataDate = {
+      newStart: Date.now(),
+      newEnd: dateEnd
+    }
+
+    const requestOptions = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(dataDate)
+    }
+
+    fetch(`http://localhost:8080/goal/resume/${goalId}`, requestOptions)
+      .then(response => response.json())
+      .then(() => {
+        setGoals(goals => {
+          const goalDone = goals.findIndex(goal => goal._id === goalId)
+          goals[goalDone].end = false
+          goals[goalDone].todayDone = false
+
+          return [...goals]
+        })
+      })
+      .catch(err => console.error(err))
+  }
+
+  const Done = goalId => {
+    // fetch
+    const requestOptions = {
+      method: 'PATCH',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ todayDone: true })
+    }
+
+    fetch(`http://localhost:8080/goal/${goalId}`, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        setGoals(goals => {
+          const goalDone = goals.findIndex(goal => goal._id === goalId)
+          goals[goalDone].todayDone = true
+
+          return [...goals]
+        })
+      })
+      .catch(err => console.error(err))
+  }
+
+  const Drop = ({ goalId, start }) => {
+    const dataGoalAbandon = {
+      end: true,
+      newTries: {
+        start,
+        end: new Date()
+      }
+    }
+    const requestOptions = {
+      method: 'PUT',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(dataGoalAbandon)
+    }
+
+    fetch(`http://localhost:8080/goal/${goalId}`, requestOptions)
+      .then(response => response.json())
+      .then(data => {
+        setGoals(goals => {
+          const goalDone = goals.findIndex(goal => goal._id === goalId)
+          goals[goalDone].tries = data.tries
+          goals[goalDone].end = true
+
+          return [...goals]
+        })
+      })
+      .catch(err => console.error(err))
+  }
+  return {
+    RemoveGoal,
+    ResumeGoal,
+    Done,
+    Drop,
+    goals,
+    loading,
+    error
+  }
+}
